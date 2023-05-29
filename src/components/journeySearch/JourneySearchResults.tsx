0d567b5a039db9619e1de8f4c090cd50db3b46cc
Tong Zheng Hong <zhtong315@gmail.com>
2023-05-30 01:14:04 +0800

    Add search journeys feature

---
+++ src/components/journeySearch/JourneySearchResults.tsx
@@ -0,0 +1,30 @@
+import { Journey, User } from "@prisma/client";
+import JourneySearchItem from "./JourneySearchItem";
+
+export default function JourneySearchResults({
+  searchResults,
+}: {
+  searchResults: (Journey & { createdBy: User })[];
+}) {
+  return (
+    <>
+      {searchResults.length === 0 && (
+        <p className="w-full text-center">No journeys found!</p>
+      )}
+      {searchResults.length > 0 && (
+        <>
+          <p className="ml-4">Found {searchResults.length} journeys:</p>
+          <ul key="Journey Search List">
+            {searchResults.map((journey) => {
+              return (
+                <li>
+                  <JourneySearchItem journey={journey} />
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+    </>
+  );
+}
