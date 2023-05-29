a5fcdea24b682aa0b1a9d0f4aa865ae43515092c
mcmc101001 <tohmingchun101001@gmail.com>
2023-05-30 00:09:38 +0800

    test

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "./ui/Input";
 import QuestItem from "./QuestItem";
-import { number } from "zod";
 
 type Place = {
-  lat: number,
-  lng: number,
-  name: string,
-}
+  lat: number;
+  lng: number;
+  name: string;
+};
 
 export default function SearchResults({
   journeyId,
@@ -19,48 +18,50 @@ export default function SearchResults({
   userId: string;
 }) {
   let [searchInput, setSearchInput] = useState("");
-  
 
   // Latitude & Longitude
-  let lat: number | undefined
-  let lng: number | undefined
+  let lat: number | undefined;
+  let lng: number | undefined;
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      lat = position.coords.latitude
-      lng = position.coords.longitude
-    })
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+    });
   }
   if (lat == undefined || lng == undefined) {
-    lat = 1.35
-    lng = 103.8
+    lat = 1.35;
+    lng = 103.8;
   }
 
   // Search radius
-  const rad = 4 * 1000
+  const rad = 4 * 1000;
 
-  let places: Place[] = []
+  let places: Place[] = [];
   useEffect(() => {
     async function updatePlaces() {
-      const newPlaces: Place[] = []
-      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${rad}$key=${process.env.NEXTGOOGLE_PLACE_API_KEY!}`
+      const newPlaces: Place[] = [];
+      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${rad}$key=
+      ${process.env.NEXTGOOGLE_PLACE_API_KEY!}`;
       await fetch(url)
-        .then(res => res.json())
-        .then(json => {
+        .then((res) => res.json())
+        .then((json) => {
           for (let googlePlace of json.results) {
             let place = {
               lat: googlePlace.geometry.location.lat,
               lng: googlePlace.geometry.location.lng,
               name: googlePlace.name,
-            }
+            };
             newPlaces.push(place);
           }
           if (searchInput !== "") {
-            newPlaces.filter(place => place.name.includes(searchInput))
+            newPlaces.filter((place) => place.name.includes(searchInput));
           }
-          places = newPlaces
-        })
+          places = newPlaces;
+        });
     }
-  }, [searchInput])
+    updatePlaces();
+    console.log("places", places);
+  }, [searchInput]);
 
   return (
     <div className="w-full">
@@ -72,7 +73,7 @@ export default function SearchResults({
       />
       {searchInput !== "" && (
         <div className="w-full h-full flex flex-col items-center bg-background1 p-2 border-b-2 border-background2">
-          {places.map(place =>
+          {places.map((place) => (
             <QuestItem
               action={"PLAY"}
               addable={true}
@@ -83,7 +84,7 @@ export default function SearchResults({
               points={2}
               userId={userId}
             />
-          )}
+          ))}
           <QuestItem
             action={"SHOP"}
             addable={true}
