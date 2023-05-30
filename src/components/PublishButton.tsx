6d2b4a93df0598cfb959bd2a9d10b98f3a3904bc
mcmc101001 <tohmingchun101001@gmail.com>
2023-05-30 11:24:14 +0800

    fixed bug

--- src/components/PublishButton.tsx
+++ src/components/PublishButton.tsx
@@ -2,15 +2,16 @@
 
 import { toast } from "react-hot-toast";
 import Button from "./ui/Button";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function PublishButton() {
+  let router = useRouter();
   return (
     <Button
       className="py-6 text-xl border-2 border-background2 mb-12"
       onClick={() => {
         toast.success("Journey published!");
-        redirect("/welcome");
+        router.push("/welcome");
       }}
     >
       Publish
